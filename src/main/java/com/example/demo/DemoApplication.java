984ec4e05e2e57340360c919ffb852e319b9c537
MihaiUM2001 <mihai-alexandru.udrescu-milosav@student.upt.ro>
2024-03-09 00:13:38 +0200

    Fixed requested issues.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.sql.SQLOutput;
-
 @SpringBootApplication
 public class DemoApplication {
 
