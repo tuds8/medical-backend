02780a766528c6277f19e681df191b66a586b112
MihaiUM2001 <mihai-alexandru.udrescu-milosav@student.upt.ro>
2024-03-08 23:52:59 +0200

    Trying git!

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,14 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Hackaton");
 	}
 
 }
