73fa111244d1e49541df93e2c8f137162654a252
MihaiUM2001 <mihai-alexandru.udrescu-milosav@student.upt.ro>
2024-03-10 00:08:33 +0200

    Ora 12 commit

--- src/main/java/Controllers/UserController.java
+++ src/main/java/Controllers/UserController.java
@@ -9,13 +9,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value="/api/v1/user/")
 public class UserController {
 
     @Autowired
     UserService userService;
-    @PostMapping("/user")
+    @PostMapping("/create")
     public void createUser(@RequestBody User user){
         System.out.println("create");
         userService.createUser(user);
     }
 }
+
+/*
+@RestController
+@RequestMapping(value="/api/v1/posts/")
+public interface PostController {
+    //CREATE
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity.BodyBuilder addPostToUser(@PathVariable String user_id, @RequestBody PostDTO postDTO);
+
+    //READ
+    @GetMapping(value="/{user_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    List<PostDTO> getMyPosts(@PathVariable String user_id);
+ */
