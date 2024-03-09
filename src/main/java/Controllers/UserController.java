d5815682cecb9a35130a73b7a4e143eb38c7a98b
MihaiUM2001 <mihai-alexandru.udrescu-milosav@student.upt.ro>
2024-03-10 01:01:30 +0200

    ora 1

--- src/main/java/Controllers/UserController.java
+++ src/main/java/Controllers/UserController.java
@@ -2,34 +2,19 @@ package Controllers;
 
 import Models.User;
 import Service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 @RestController
-@RequestMapping(value="/api/v1/user/")
+@RequestMapping(value="/api/v1/user")
+@RequiredArgsConstructor
 public class UserController {
 
-    @Autowired
-    UserService userService;
-    @PostMapping("/create")
+    private final UserService userService;
+    @PutMapping(value = "/create", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void createUser(@RequestBody User user){
         System.out.println("create");
         userService.createUser(user);
     }
 }
-
-/*
-@RestController
-@RequestMapping(value="/api/v1/posts/")
-public interface PostController {
-    //CREATE
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity.BodyBuilder addPostToUser(@PathVariable String user_id, @RequestBody PostDTO postDTO);
-
-    //READ
-    @GetMapping(value="/{user_id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    List<PostDTO> getMyPosts(@PathVariable String user_id);
- */
